ef56c0eeb13ca269ba4880a70a10822ea25bc4d2
Stanley-Kemuel Lloyd Salvation <skemuel007@gmail.com>
2023-12-12 21:21:29 +0100

    Ensure database migration

--- Outbox.Order/Program.cs
+++ Outbox.Order/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Outbox.Order.Data;
+using Outbox.Order.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,9 @@ var app = builder.Build();
 builder.Services.AddDbContext<OrderContext>(opt => 
     opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")), ServiceLifetime.Singleton);
 
+builder.Services.AddHostedService<OutboxPublisher>();
+builder.Services.AddSingleton<IPublisher, Publisher>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -20,6 +24,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+try
+{
+    var context = app.Services.GetService<OrderContext>();
+    await context.Database.MigrateAsync();
+}
+catch(Exception ex)
+{
+    app.Logger.LogError(ex.ToString());
+}
 
 app.Run();
